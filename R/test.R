55dd9da8bab22e84881b51ff2e3e61e122e278db
ewenger@idmod.org <ewenger@idmod.org>
2019-12-27 12:36:57 -0800

    Add test script

---
+++ R/test.R
@@ -0,0 +1,19 @@
+require(here)
+require(Rcpp)
+
+sourceCpp(here("src", "Cohort.cpp"))
+
+c1 = new(Cohort, 10)
+
+c1$count = c1$count + 10
+
+c1$print()
+
+x = c()
+for (i in 1:1000)
+{
+  x[i] = c1$drawFraction(0.2)
+}
+
+hist(x)
+
